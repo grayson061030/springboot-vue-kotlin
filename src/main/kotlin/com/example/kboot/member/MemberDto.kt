d4a46f6811d2fbd8d6c12e1b15a615fe61d64b83
gavinkim <gavinkim@coupang.com>
2019-05-05 00:14:43 +0900

    null 일 경우 직렬화에서 제거하도록 수정

--- src/main/kotlin/com/example/kboot/member/MemberDto.kt
+++ src/main/kotlin/com/example/kboot/member/MemberDto.kt
@@ -1,8 +1,11 @@
 package com.example.kboot.member
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class MemberDto(
         val firstName: String,
         val lastName: String,
         val age: Int,
-        val email: String
+        val email: String?=null
 )
